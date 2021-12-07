09e8327cb18ad0ea0014c475cb0eb7a5f8171f82
ogmobot <ogmobot@github.com>
2021-12-07 16:38:30 +1100

    Day 7 solution in Rust

--- day07.rs
+++ day07.rs
@@ -1,4 +1,32 @@
-// compile with rustc
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn triangular(n: i32) -> i32 {
+    return n*(n+1)/2;
 }
+
+fn main() {
+    let contents = fs::read_to_string("input07.txt")
+        .expect("File read error");
+    let vals: Vec<i32> = contents
+        .split(",")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.trim().parse().unwrap())
+        .collect();
+    // part 1
+    let result_p1: i32 = (0..(*vals.iter().max().unwrap()))
+        .map(|target| vals
+            .iter()
+            .map(|val| (val - target).abs())
+            .sum())
+        .min().unwrap();
+    println!("{}", result_p1);
+    // part 2
+    let result_p2: i32 = (0..(*vals.iter().max().unwrap()))
+        .map(|target| vals
+            .iter()
+            .map(|val| triangular((val - target).abs()))
+            .sum())
+        .min().unwrap();
+    println!("{}", result_p2);
+}
+// not 96003565
