d71bf44cf9c7ae8c6c17f06366a3f164e623cce6
ogmobot <ogmobot@github.com>
2021-12-09 20:30:23 +1100

    Rewrite day 9 to try to improve efficiency

--- day09.go
+++ day09.go
@@ -13,31 +13,32 @@ type coord struct {
     col int
 }
 
-func adjacent_coords(array []string, row int, col int) []coord {
+func adjacent_coords(array *([]string), row int, col int) []coord {
     var result []coord
-    if row > 0                { result = append(result, coord{row - 1, col}) }
-    if (row + 1) < len(array) { result = append(result, coord{row + 1, col}) }
-    if col > 0                { result = append(result, coord{row, col - 1}) }
-    if (col + 1) < len(array) { result = append(result, coord{row, col + 1}) }
+    if row > 0                 { result = append(result, coord{row - 1, col}) }
+    if (row + 1) < len(*array) { result = append(result, coord{row + 1, col}) }
+    if col > 0                 { result = append(result, coord{row, col - 1}) }
+    if (col + 1) < len((*array)[row]) {
+                                 result = append(result, coord{row, col + 1}) }
     return result
 }
 
-func risk(array []string, row int, col int) int {
+func risk(array *([]string), row int, col int) int {
     lowpoint := true
     adjs := adjacent_coords(array, row, col)
     for i := 0; i < len(adjs); i++ {
-        lowpoint = lowpoint && (array[row][col] < array[adjs[i].row][adjs[i].col])
+        lowpoint = lowpoint && ((*array)[row][col] < (*array)[adjs[i].row][adjs[i].col])
     }
     if lowpoint {
-        return 1 + (int(array[row][col]) - int('0'))
+        return 1 + (int((*array)[row][col]) - int('0'))
     }
     return 0
 }
 
 func find_attractor(array []string, row int, col int) coord {
     var adjs []coord
-    for risk(array, row, col) == 0 {
-        adjs = adjacent_coords(array, row, col)
+    for risk(&array, row, col) == 0 {
+        adjs = adjacent_coords(&array, row, col)
         for i := 0; i < len(adjs); i++ {
             if array[adjs[i].row][adjs[i].col] < array[row][col] {
                 row, col = adjs[i].row, adjs[i].col
@@ -48,8 +49,29 @@ func find_attractor(array []string, row int, col int) coord {
     return coord{row, col}
 }
 
+func basin_size(array *([]string), lowpoint coord, result chan int) {
+    // because every point drains to a single lowpoint, each basin must be bounded by 9s
+    visited := make(map[coord]bool)
+    var todo []coord
+    var adjs []coord
+    var popped coord
+    todo = append(todo, lowpoint)
+    for len(todo) > 0 {
+        popped, todo = todo[0], todo[1:]
+        visited[popped] = true
+        adjs = adjacent_coords(array, popped.row, popped.col)
+        for i := 0; i < len(adjs); i++ {
+            r, c := adjs[i].row, adjs[i].col
+            if (!visited[coord{r, c}]) && (int((*array)[r][c]) != int('9')) {
+                todo = append(todo, coord{r, c})
+            }
+        }
+    }
+    result <- len(visited)
+}
+
 func main() {
-    inputfile, err := os.Open("input09.txt")
+    inputfile, err := os.Open("input09.orig")
     if err != nil { panic(err) }
 
     var seafloor []string
@@ -58,28 +80,30 @@ func main() {
         seafloor = append(seafloor, strings.Trim(scanner.Text(), "\n"))
     }
     // don't both converting 48..57 to 0..9
+    var lowpoints []coord
     // part 1
     total := 0
     for row := 0; row < len(seafloor); row++ {
         for col := 0; col < len(seafloor[row]); col++ {
-            total += risk(seafloor, row, col)
+            risk_level := risk(&seafloor, row, col)
+            total += risk_level
+            if risk_level > 0 {
+                lowpoints = append(lowpoints, coord{row, col})
+            }
         }
     }
     fmt.Println(total)
     // part 2
-    basins := make(map[coord]int)
-    for row := 0; row < len(seafloor); row++ {
-        for col := 0; col < len(seafloor[row]); col++ {
-            if seafloor[row][col] != '9' {
-                basins[find_attractor(seafloor, row, col)] += 1
-            }
-        }
-    }
     var sizes []int
-    for _, size := range basins {
-        sizes = append(sizes, size)
+    results := make(chan int)
+    for i := 0; i < len(lowpoints); i++ {
+        go basin_size(&seafloor, lowpoints[i], results)
+    }
+    for i := 0; i < len(lowpoints); i++ {
+        sizes = append(sizes, (<-results))
     }
     sort.Slice(sizes, func (i, j int) bool { return sizes[i] > sizes[j] })
+    //fmt.Println(sizes)
     fmt.Println(sizes[0] * sizes[1] * sizes[2])
 }
 
