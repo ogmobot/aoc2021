7ea811196c5c9a70178bc6ae509c22a1baa9fbda
ogmobot <ogmobot@github.com>
2021-12-11 15:59:34 +1100

    File reads in Elixir

--- day.exs
+++ day.exs
@@ -1,5 +1,8 @@
 #!/usr/bin/elixir
 
-text = File.read!("input.txt")
-IO.puts(text)
+[_ | lines] = File.read!("input.txt")
+    |> String.split("\n", trim: true)
+nums = lines
+    |> Enum.map(fn line -> String.split(line, ",") end)
+IO.puts(nums)
 
