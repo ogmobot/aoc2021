bdf520b62ecb142db68eea4beb789ad06f4158e9
ogmobot <ogmobot@github.com>
2021-12-11 21:57:33 +1100

    Minor day 11 cleanup

--- day11.jl
+++ day11.jl
@@ -1,61 +1,52 @@
-function adjs(n)
+function adjs(n, dims)
     # returns indices that are adjacent to n
-    # (in a 10 x 10 grid)
-    # usually n+1, n-1, n+10, n-10
+    # in a 2D grid with dimension `dims`
+    height, width = dims
     result = []
     isleft = true
     isright = true
-    istop = true
-    isbottom = true
     if n % 10 != 1
         isleft = false
         append!(result, [n-1])
     end
-    if n % 10 != 0
+    if n % width != 0
         isright = false
         append!(result, [n+1])
     end
-    if n > 10
-        istop = false
-        append!(result, [n-10])
+    if n > width
+        append!(result, [n-width])
+        if !isleft
+            append!(result, [n-width-1])
+        end
+        if !isright
+            append!(result, [n-width+1])
+        end
     end
-    if n <= 90
-        isbottom = false
-        append!(result, [n+10])
-    end
-    if (!isbottom) && (!isleft)
-        append!(result, [n+9]) # lower-left
-    end
-    if (!isbottom) && (!isright)
-        append!(result, [n+11]) # lower-right
-    end
-    if (!istop) && (!isleft)
-        append!(result, [n-11]) # upper-left
-    end
-    if (!istop) && (!isright)
-        append!(result, [n-9]) # upper-right
+    if n <= (height-1)*width
+        append!(result, [n+width])
+        if !isleft
+            append!(result, [n+width-1])
+        end
+        if !isright
+            append!(result, [n+width+1])
+        end
     end
     result
 end
 
-function gainenergy(grid)
+function update(grid)
     # grid is mutable :)
-    for (index, value) in enumerate(grid)
-        grid[index] = grid[index] + 1
-    end
-end
-
-function flash(grid)
     fullcharge = []
     flashed = []
     for (index, value) in enumerate(grid)
-        if value == 10
+        if value == 9
             append!(fullcharge, index)
         end
+        grid[index] = grid[index] + 1
     end
     while !isempty(fullcharge)
         index = pop!(fullcharge)
-        for i in adjs(index)
+        for i in adjs(index, size(grid))
             grid[i] += 1
             if grid[i] == 10 # don't have to worry about adding same one twice
                 append!(fullcharge, i)
@@ -72,12 +63,10 @@ end
 function main()
     lines = map(collect, readlines("input11.txt"))
     grid  = map(x->parse(Int, x), permutedims(hcat(lines...)))
-
     total = 0
     timer = 0
     while true
-        gainenergy(grid)
-        subtotal = flash(grid)
+        subtotal = update(grid)
         total = total + subtotal
         timer += 1
         if timer == 100
