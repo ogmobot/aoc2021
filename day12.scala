a8372612863ba89aec45a5207f8bb8062fab049e
ogmobot <ogmobot@github.com>
2021-12-12 17:16:27 +1100

    Clean up day 12 solution

--- day12.scala
+++ day12.scala
@@ -2,25 +2,20 @@
 !#
 import scala.io.Source
 
-def find_connections(pairs: List[List[String]], a: String): List[String] = {
-    if (pairs.isEmpty) {
-        return List[String]()
-    } else if (pairs.head(0) == a) {
-        return pairs.head(1) +: find_connections(pairs.tail, a)
-    } else if (pairs.head(1) == a) {
-        return pairs.head(0) +: find_connections(pairs.tail, a)
-    } else {
-        return find_connections(pairs.tail, a)
-    }
+def find_connections(pairs: List[List[String]], a: String): List[String] =
+    pairs match {
+    case p :: tail if p(0) == a => p(1) +: find_connections(tail, a)
+    case p :: tail if p(1) == a => p(0) +: find_connections(tail, a)
+    case p :: tail              =>         find_connections(tail, a)
+    case _                      => Nil
 }
 
 def count_paths(
-    start: String,
-    end: String,
+    start: String, end: String,
     connections: List[List[String]],
     visited: List[String],
     lenient: Boolean
-): Long = {
+): Long =
     find_connections(connections, start)
     .filter(
         x => (!visited.contains(x)) || (lenient && visited.count(_ == x) == 1))
@@ -35,15 +30,15 @@ def count_paths(
                     (start +: visited).filter(xx => xx.exists(_.isLower)),
                     lenient && (!visited.contains(x)))}})
     .sum
-}
 
 def main() = {
     val connections = Source.fromFile("input12.txt").getLines().map(
         _.split("-").toList
     ).toList
-    val part1 = count_paths("start", "end", connections, List[String](), false)
-    val part2 = count_paths("start", "end", connections, "start" :: Nil, true)
-    println(s"$part1\n$part2")
+    // part 1
+    println(count_paths("start", "end", connections, List[String](), false))
+    // part 2
+    println(count_paths("start", "end", connections, List("start"), true))
 }
 
 main()
