b1e1657c56c06208c838aa1622fcd723c447065a
ogmobot <ogmobot@github.com>
2021-12-13 19:23:44 +1100

    Comments on Haskell

--- day13.hs
+++ day13.hs
@@ -3,8 +3,8 @@ import System.IO
 import Data.List.Split
 import Data.List
 
-data PaperFold = FoldX Int | FoldY Int deriving Show
-data Point     = Point Int Int deriving (Show, Eq)
+data PaperFold = FoldX Int | FoldY Int | NoFold
+data Point     = Point Int Int deriving Eq
 
 foldPoint :: PaperFold -> Point -> Point
 foldPoint (FoldX coord) (Point x y)
@@ -13,24 +13,25 @@ foldPoint (FoldX coord) (Point x y)
 foldPoint (FoldY coord) (Point x y)
     | coord >= y = Point x y
     | coord <  y = Point x ((2 * coord) - y)
+foldPoint NoFold p = p
 
 part1 :: [Point] -> [PaperFold] -> Int
-part1 xs (f:fs) = length (nub (map (foldPoint f) xs))
+part1 xs (f:fs) = length . nub $ map (foldPoint f) xs
 
-solve :: [Point] -> [PaperFold] -> [Point]
-solve xs (f:fs) = solve (map (foldPoint f) xs) fs
-solve xs []     = xs
+part2 :: [Point] -> [PaperFold] -> [Point]
+part2 xs (f:fs) = part2 (map (foldPoint f) xs) fs
+part2 xs []     = xs
 
 parseCoord :: String -> Point
 parseCoord line = Point x y
-    where x:y:_ = (map read $ splitOn "," line)
+    where x:y:_ = map read $ splitOn "," line
 
 parseFold :: String -> PaperFold
 parseFold line
     | axis == ['x'] = FoldX num
     | axis == ['y'] = FoldY num
-    | otherwise     = FoldX 999999 --won't change any points
-    where num  = read (drop 13 line)
+    | otherwise     = NoFold
+    where num  = read $ drop 13 line
           axis = take 1 (drop 11 line)
 
 extractPoints :: String -> [Point]
@@ -53,16 +54,24 @@ makeStrings ((Point x y):ps) buffer =
 
 newBuffer :: [Point] -> [String]
 newBuffer [] = ["."]
-newBuffer ps = (map (\x -> (map (\x -> ' ') [1..maxx+1])) [1..maxy+1])
+newBuffer ps = (map (\x -> (map (\x -> ' ') [0..maxx])) [0..maxy])
     where maxx = maximum (map (\(Point x y) -> x) ps)
           maxy = maximum (map (\(Point x y) -> y) ps)
 
 main = do
     contents <- readFile "input13.txt"
     let
-        points = extractPoints contents
-        folds  = extractFolds contents
-        resultOne = part1 points folds
-        resultTwo = solve points folds
-    print resultOne
-    mapM_ putStrLn (makeStrings resultTwo (newBuffer resultTwo))
+        points  = extractPoints contents
+        folds   = extractFolds contents
+        result1 = part1 points folds
+        result2 = part2 points folds
+    print result1
+    mapM_ putStrLn (makeStrings result2 (newBuffer result2))
+
+{-
+Oh man. I forgot how fussy Haskell is. The core logic of this program (i.e. the
+`foldPoint` function) was done in minutes, and Haskell's type system ensured it
+was safe; but it took over an hour to organise the input and output of the
+program. (To be fair, if I had used the language at all in the last few months,
+I probably would have found it a lot easier.)
+-}
