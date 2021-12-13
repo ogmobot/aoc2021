9f058e21af99cdf071c9f85d84dc14d488b86c18
ogmobot <ogmobot@github.com>
2021-12-13 21:52:46 +1100

    Update benchmark script

--- bench.sh
+++ bench.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 /usr/bin/time -v $@ 2>&1 |
-    grep -e "Maximum resident set size" -e "User time" -e "System time"
+    grep -e "Maximum resident set size" \
+         -e "User time" \
+         -e "System time" \
+         -e "Elapsed (wall clock) time"
