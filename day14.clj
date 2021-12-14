48c083d1213981a4113139b2b572108c6e77c3ec
ogmobot <ogmobot@github.com>
2021-12-14 20:13:12 +1100

    The fast version of day 14 won't overflow the stack, so get rid of `recur`

--- day14.clj
+++ day14.clj
@@ -2,17 +2,12 @@
 (require '[clojure.string :as str])
 
 (defn dofreqsubs [fstable pairfreqs]
-    (loop [pairs (keys pairfreqs)
-           acc {}]
-        (if (= 0 (count pairs))
-            acc
-            (recur
-                (rest pairs)
-                (let [pair (first pairs)
-                    amount (pairfreqs pair)]
-                    (merge-with + acc
-                        (apply merge
-                            (map #(hash-map % amount) (fstable pair)))))))))
+    (if (= 0 (count pairfreqs))
+        {}
+        (let [[pair amount] (first pairfreqs)]
+            (merge-with +
+                (apply merge (map #(hash-map % amount) (fstable pair)))
+                (dofreqsubs fstable (rest pairfreqs))))))
 
 (defn make-substable [lines]
     (if (> (count lines) 0)
@@ -21,36 +16,26 @@
             (make-substable (rest lines)))
         nil))
 
-
 (defn make-frequency-substable [substable]
-    (let [sub-keys (keys substable)]
-        (loop [index 0
-               acc {}]
-            (if (= index (count sub-keys))
-                acc
-                (recur
-                    (inc index)
-                    (let [k (nth sub-keys index)
-                          v (substable k)]
-                        (merge acc
-                            {(list (first k) (second k))
-                                (list
-                                    (list (first k) (first v))
-                                    (list (first v) (second k)))})))))))
+    (if (= 0 (count substable))
+        {}
+        (let [[k v] (first substable)]
+            (merge
+                {(list (first k) (second k))
+                    (list
+                        (list (first k) (first v))
+                        (list (first v) (second k)))}
+            (make-frequency-substable (rest substable))))))
 
 (defn make-pair-frequencies [string]
     (frequencies (partition 2 1 string)))
 
 (defn determine-freqs [pairfreqs lastchar]
-    (loop [pairfreqs pairfreqs
-           acc {}]
-        (if (= 0 (count pairfreqs))
-            (merge-with + acc {lastchar 1})
-            (recur
-                (rest pairfreqs)
-                (merge-with + acc
-                    {(first (first (first pairfreqs)))
-                     (second (first pairfreqs))})))))
+    (if (= 0 (count pairfreqs))
+        {lastchar 1}
+        (merge-with +
+            {(first (first (first pairfreqs))) (second (first pairfreqs))}
+            (determine-freqs (rest pairfreqs) lastchar))))
 
 (defn main []
     (let [file-contents (slurp "input14.txt")
