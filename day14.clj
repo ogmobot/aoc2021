26a49b1b6f510fb7d062003a637ae0461e180f20
ogmobot <ogmobot@github.com>
2021-12-14 20:00:41 +1100

    Use fast method for both parts of day 14

--- day14.clj
+++ day14.clj
@@ -1,21 +1,7 @@
 #!/usr/bin/clojure
 (require '[clojure.string :as str])
 
-(defn dosubs [substable string]
-    ;; naive approach
-    (loop [index 0
-           acc ""]
-        (if (= (inc index) (count string))
-            (str acc (get string index))
-            (recur
-                (inc index)
-                (str
-                    acc
-                    (get string index)
-                    (substable (subs string index (+ index 2))))))))
-
 (defn dofreqsubs [fstable pairfreqs]
-    ;; scalable approach
     (loop [pairs (keys pairfreqs)
            acc {}]
         (if (= 0 (count pairs))
@@ -25,7 +11,8 @@
                 (let [pair (first pairs)
                     amount (pairfreqs pair)]
                     (merge-with + acc
-                        (apply merge (map #(hash-map % amount) (fstable pair)))))))))
+                        (apply merge
+                            (map #(hash-map % amount) (fstable pair)))))))))
 
 (defn make-substable [lines]
     (if (> (count lines) 0)
@@ -47,9 +34,6 @@
                           v (substable k)]
                         (merge acc
                             {(list (first k) (second k))
-                                ;; Note -- this assumes nothing like
-                                ;; XX -> X
-                                ;; occurs in the input.
                                 (list
                                     (list (first k) (first v))
                                     (list (first v) (second k)))})))))))
@@ -58,7 +42,6 @@
     (frequencies (partition 2 1 string)))
 
 (defn determine-freqs [pairfreqs lastchar]
-    ;; e.g. (A B A B) => {(A B) 2, (B A) 1} => first letters 2*A 1*B + lastchar
     (loop [pairfreqs pairfreqs
            acc {}]
         (if (= 0 (count pairfreqs))
@@ -66,25 +49,21 @@
             (recur
                 (rest pairfreqs)
                 (merge-with + acc
-                    {(first (first (first pairfreqs))) (second (first pairfreqs))})))))
+                    {(first (first (first pairfreqs)))
+                     (second (first pairfreqs))})))))
 
 (defn main []
     (let [file-contents (slurp "input14.txt")
           [initial-string _ & subs-list] (str/split file-contents #"\n")
-          substable (make-substable subs-list)]
+          initial-freqs (make-pair-frequencies initial-string)
+          substable (make-substable subs-list)
+          fstable (make-frequency-substable substable)
+          results (iterate (partial dofreqsubs fstable) initial-freqs)]
         ;; part 1
-        (let [result-string (->
-                        (iterate (partial dosubs substable) initial-string)
-                        (nth 10))
-              freqs (frequencies result-string)]
+        (let [freqs (determine-freqs (nth results 10) (last initial-string))]
             (println (- (apply max (vals freqs)) (apply min (vals freqs)))))
         ;; part 2
-        (let [initial-freqs (make-pair-frequencies initial-string)
-              fstable (make-frequency-substable substable)
-              result-table (->
-                        (iterate (partial dofreqsubs fstable) initial-freqs)
-                        (nth 40))
-              freqs (determine-freqs result-table (last initial-string))]
+        (let [freqs (determine-freqs (nth results 40) (last initial-string))]
             (println (- (apply max (vals freqs)) (apply min (vals freqs)))))))
 
 (main)
