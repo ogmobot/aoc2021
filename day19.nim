f14bd4c9390159e3c3803d2b589fbb5c2d9f0e15
ogmobot <ogmobot@github.com>
2021-12-20 09:37:03 +1100

    Day 19 solution in Nim

--- day19.nim
+++ day19.nim
@@ -2,21 +2,24 @@ import std/sets
 import std/sequtils
 import std/strutils
 
-#const PROBE_RANGE = 1000
+const PROBE_RANGE = 1000
 const TOLERANCE = 12
 
 type
-    Point = tuple[x, y, z: int]
-    Probe = HashSet[Point]
+    Point     = tuple[x, y, z: int]
+    ProbeData = tuple[loc: Point, facing: int]
+    Probe     = tuple[
+        beacons: HashSet[Point],
+        probelocs: HashSet[Point]]
 
 ### program logic ###
 
-#func add_points(a: Point, b: Point): Point =
-    #(a[0] + b[0], a[1] + b[1], a[2] + b[2])
+func add_points(a: Point, b: Point): Point =
+    (a[0] + b[0], a[1] + b[1], a[2] + b[2])
 func sub_points(a: Point, b: Point): Point =
     (a[0] - b[0], a[1] - b[1], a[2] - b[2])
-#func in_range(a: Point, b: Point): bool =
-    #abs(a[0] - b[0]) <= PROBE_RANGE and abs(a[1] - b[1]) <= PROBE_RANGE and abs(a[2] - b[2]) <= PROBE_RANGE
+func in_range(a: Point, b: Point): bool =
+    abs(a[0] - b[0]) <= PROBE_RANGE and abs(a[1] - b[1]) <= PROBE_RANGE and abs(a[2] - b[2]) <= PROBE_RANGE
 
 func do_rotation(p: Point, i: int): Point =
     let (x, y, z) = p
@@ -25,8 +28,8 @@ func do_rotation(p: Point, i: int): Point =
         (-x, z, y), (-x, y, -z), (-x, -z, -y), (-x, -y, z),
         ( y, z, x), ( y, x, -z), ( y, -z, -x), ( y, -x, z),
         (-y, x, z), (-y, z, -x), (-y, -x, -z), (-y, -z, x),
-        ( z, x, y), ( z, y, -x), ( z, -y, -x), ( z, -x, y),
-        (-z, y, x), (-z, x, -y), (-z, -x, -y), (-z, -y, x)
+        ( z, x, y), ( z, y, -x), ( z, -x, -y), ( z, -y, x),
+        (-z, y, x), (-z, x, -y), (-z, -y, -x), (-z, -x, y)
     ][i]
 
 func do_unrotation(p: Point, i: int): Point =
@@ -36,76 +39,85 @@ func do_unrotation(p: Point, i: int): Point =
         (-x, z, y), (-x,  y,-z), (-x, -z, -y), (-x, -y, z),
         ( z, x, y), ( y,  x,-z), (-z,  x, -y), (-y,  x, z),
         ( y,-x, z), (-z, -x, y), (-y, -x, -z), ( z, -x,-y),
-        ( y, z, x), (-z,  y, x), (-z, -y,  x), (-y,  z, x),
-        ( z, y,-x), ( y, -z,-x), (-y, -z, -x), ( z, -y,-x)
+        ( y, z, x), (-z,  y, x), (-y, -z,  x), ( z, -y, x),
+        ( z, y,-x), ( y, -z,-x), (-z, -y, -x), (-y,  z,-x)
     ][i]
 
 for i in countup(0, 23):
-    assert do_unrotation(do_rotation((1000, 200, 30), i), i) == (1000, 200, 30)
+    assert do_unrotation(do_rotation((1000, 200, 30), i), i) == (1000, 200, 30), $i
+    assert do_rotation(do_unrotation((1000, 200, 30), i), i) == (1000, 200, 30), $i
 
 
-func it_all_lines_up(probe_a: Probe, probe_b: Probe, r: int, offset: Point): bool =
+func it_all_lines_up(probe_a: Probe, probe_b: Probe, b_facing: int, b_loc: Point): bool =
     #[
-    Asserts that if probe b has `offset` and rotation `r` relative to probe_a,
-    there are at least 12 points in common.
+    Asserts that if each point in probe_a is transformed, it matches probe_b.
+    There must be at least TOLERANCE points in common.
     ]#
     var beacon_count = 0
-    for p in probe_b:
-        let new_p = sub_points(do_rotation(p, r), offset)
-        # this is how the point in a *should* appear to probe b
-        #if in_range((0, 0, 0), new_p):
-        if (new_p in probe_a):
+    for b in probe_b.beacons:
+        let new_b = do_rotation(sub_points(b, b_loc), b_facing)
+        # this is how the point in b *should* appear to probe a
+        if (new_b in probe_a.beacons):
             beacon_count += 1
-        if beacon_count >= TOLERANCE:
-            return true
-    #else:
+            #debugecho "old_b=", $b, " new_b=", $new_b, " is in probe_a"
+        # probe_a's location is 0, 0, 0
+        #elif in_range((0, 0, 0), new_b) and (not (new_b in probe_a)):
+            #return false
+    if beacon_count >= TOLERANCE:
+        return true
     return false
 
-func line_up_probes(probe_a: Probe, probe_b: Probe): tuple[p: Point, r: int] =
+func line_up_probes(probe_a: Probe, probe_b: Probe): ProbeData =
     #[
     Lines up two probes by finding TOLERANCE+ points in common.
-    Returns the offset or [0, 0, 0], 0 on failure
+    Returns the offset/facing of probe_b or [0, 0, 0], 0 on failure
     ]#
-    for a in probe_a:
-        for b in probe_b:
-            for r in countup(0, 23):
-                let potential_offset = sub_points(do_rotation(b, r), a)
-                if it_all_lines_up(probe_a, probe_b, r, potential_offset):
-                    return (potential_offset, r)
+    for a in probe_a.beacons:
+        for b in probe_b.beacons:
+            for b_facing in countup(0, 23):
+                let potential_b_loc = sub_points(b, do_unrotation(a, b_facing))
+                if it_all_lines_up(probe_a, probe_b, b_facing, potential_b_loc):
+                    return (potential_b_loc, b_facing)
     return ((0, 0, 0), 0)
 
-func transform_all_points(probe_b: Probe, offset: Point, r: int): Probe =
-    probe_b.map(
-        proc (p: Point): Point =
-            sub_points(do_rotation(p, r), offset))
+func transform_all_points(probe_b: Probe, b_loc: Point, b_facing: int): Probe =
+    (probe_b.beacons.map(
+        proc (b: Point): Point =
+            do_rotation(sub_points(b, b_loc), b_facing)),
+    # facing data isn't transformed, but probably should be
+    probe_b.probelocs.map(
+        proc (loc: Point): Point =
+            do_rotation(sub_points(loc, b_loc), b_facing)))
+            
 
-proc merge_probe_list(raw_probes: seq[Probe]): tuple[p: Probe, offsets: seq[Point]] =
+proc merge_probe_list(raw_probes: seq[Probe]): Probe =
     var probes = toSeq(raw_probes)
-    var offsets: seq[Point] = @[(0, 0, 0)]
     # find matching pair
     while probes.len > 1:
         #echo probes.map(proc (x: Probe): int = x.len)
         block outer:
-            #for i in countup(0, probes.len - 1):
-            for i in countdown(probes.len - 1, 0): # counting from top means we see
-                for j in countup(0, i - 1):        # the big sets faster
-                    let trans = line_up_probes(probes[j], probes[i])
+            for i in countup(0, probes.len - 1):
+                for j in countup(i + 1, probes.len - 1):
+                    # probes[i] is absolute position, probes[j] relative
+                    let trans = line_up_probes(probes[i], probes[j])
                     if trans != ((0, 0, 0), 0):
-                        let (offset, r) = trans
-                        echo "offset=", $offset, " r=", $r
-                        offsets.add(do_unrotation(offset, r))
-                        let megaprobe =
-                            probes[j] + transform_all_points(probes[i], offset, r)
-                        echo "Merged probes ", $i, " (len=", $probes[i].len, ") and ", $j, " (len=", $probes[j].len, ") (", $(probes.len - 1), " remaining) - megaprobe length ", megaprobe.len
-                        echo "offsets=", $offsets
-                        # i > j guaranteed
-                        probes.delete(i) # .delete preserves order, .del doesn't
+                        let (b_loc, b_facing) = trans
+                        #echo "b_loc=", $b_loc, " b_facing=", $b_facing
+                        let transformed = transform_all_points(probes[j], b_loc, b_facing)
+                        let megaprobe: Probe =
+                            (probes[i].beacons + transformed.beacons,
+                            probes[i].probelocs + transformed.probelocs)
+                        echo "Merged probes ", $i, " (len=", $probes[i].beacons.len, ") and ", $j, " (len=", $probes[j].beacons.len, ") (", $(probes.len - 1), " remaining) - megaprobe length ", megaprobe.beacons.len
+                        #echo "megaprobe.beacons=", $megaprobe.beacons
+                        #echo "megaprobe.probelocs=", $megaprobe.probelocs
+                        # j > i guaranteed
                         probes.delete(j)
+                        probes.delete(i) # .delete preserves order, .del doesn't
                         probes.add(megaprobe)
                         break outer
             echo "Couldn't merge probes :("
-            return (initHashSet[Point](), @[])
-    return (probes[0], offsets)
+            return (initHashSet[Point](), initHashSet[Point]())
+    return probes[0]
 
 func manhattan(a: Point, b: Point): int =
     abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
@@ -127,7 +139,9 @@ proc get_probes(filename: string): seq[Probe] =
     defer: f.close()
 
     var probes: seq[Probe] = @[]
-    var accumulator: Probe = initHashSet[Point]()
+    var accumulator: Probe = (
+        initHashSet[Point](),
+        toHashSet([(0, 0, 0)]))
     for raw_line in lines(filename):
         var line: string = raw_line
         line.stripLineEnd
@@ -137,10 +151,10 @@ proc get_probes(filename: string): seq[Probe] =
         elif line.len == 0:
             # record separator
             probes.add(accumulator)
-            accumulator = initHashSet[Point]()
+            accumulator = (initHashSet[Point](), toHashSet([(0, 0, 0)]))
         else:
             # data
-            accumulator.incl(parse_point(line))
+            accumulator.beacons.incl(parse_point(line))
     probes.add(accumulator)
     return probes
 
@@ -148,16 +162,17 @@ proc get_probes(filename: string): seq[Probe] =
 
 proc main(): int {.discardable.} =
     # each probe is represented by a set of ponits (the measurements it has taken)
-    let raw_probes = get_probes("input.test")
-    let probes = @[raw_probes[0], raw_probes[1], raw_probes[4]]
+    let probes = get_probes("input19.txt")
+    #let probes = @[raw_probes[0], raw_probes[1], raw_probes[4]]
     let (all_beacons, all_probes) =  merge_probe_list(probes)
     # part 1
     echo all_beacons.len
     # part 2
-    echo "offsets=", $all_probes
-    echo "\n\n", $all_beacons, "\n\n"
-    echo greatest_manhattan(all_probes)
+    echo "probelocs=", $all_probes
+    #echo "\n\n", $all_beacons, "\n\n"
+    echo greatest_manhattan(all_probes.toseq)
     return 0
 
 main()
 # 21446 is too high
+# 14147 is too low
